4ea729853f8871ab8f2d73a3e7057a2325c3aec9
wolflo <wardnick@protonmail.com>
2020-06-25 13:38:00 +0400

    Replace nonce with maxBlockNum

--- src/MiniSig.sol
+++ src/MiniSig.sol
@@ -5,6 +5,9 @@ pragma solidity 0.6.6;
 // is exceptionally bad solidity, and this should not be used except for
 // comparison with huff impl. It will also not match the huff particularly
 // well, because the huff uses an approach that can't be built in solidity.
+// Note that instead of a nonce, each signature should include a maximum
+// block number after which it is invalid. This allows the huff implementation
+// to maintain no state whatsoever.
 contract MiniSig {
 
     enum CallType {
@@ -12,16 +15,15 @@ contract MiniSig {
         DelegateCall
     }
 
-    uint256 public nonce;
-    address[] public signers;
+    address[] public signers;           // approved signers
+    uint8 public immutable threshold;   // minimum required signers
 
-    uint8 public immutable threshold;
+    // --- EIP712 ---
     bytes32 public immutable DOMAIN_SEPARATOR;
-
     bytes32 internal constant DOMAIN_SEPARATOR_TYPEHASH = keccak256("EIP712Domain(uint256 chainId,uint256 deployBlock,address verifyingContract)");
-    bytes32 internal constant EXECUTE_TYPEHASH = keccak256("Execute(uint8 callType,address target,uint256 value,uint256 nonce,bytes data)");
+    bytes32 internal constant EXECUTE_TYPEHASH = keccak256("Execute(uint8 callType,address target,uint256 value,uint256 maxBlockNum,bytes data)");
 
-    // recieve ether
+    // recieve ether, regardless of calldata
     receive () external payable {}
     fallback () external payable {}
 
@@ -52,6 +54,7 @@ contract MiniSig {
         CallType _callType,
         address _target,
         uint256 _value,
+        uint256 _maxBlockNum,
         bytes calldata _data,
         bytes calldata _sigs
     )
@@ -60,23 +63,26 @@ contract MiniSig {
     {
         // max(uint8) * 65 << max(uint256), so no overflow check
         require(_sigs.length >= uint256(threshold) * 65, "sigs-invalid-length");
+        require(block.number <= _maxBlockNum, "invalid-block");
 
-        uint256 initialNonce = nonce;
         bytes32 digest = keccak256(abi.encodePacked(
             "\x19\x01",
             DOMAIN_SEPARATOR,
             keccak256(abi.encode(
                 EXECUTE_TYPEHASH,
+                _callType,
                 _target,
                 _value,
-                initialNonce,
-                _callType,
+                _maxBlockNum,
                 keccak256(_data)
             ))
         ));
 
+        // Note: a single invalid sig will cause a revert, even if there are
+        // `>= threshold` valid sigs. But, an invalid sig after `threshold`
+        // valid sigs is ignored
         uint256 sigIdx = 0;
-        uint256 signerIdx;
+        uint256 signerIdx = 0;
         for (uint256 i = 0; i < threshold; i++) {
             // sig should be 65 bytes total, {32 byte r}{32 byte s}{1 byte v}
             address addr = ecrecover(digest, uint8(_sigs[sigIdx + 65]), _sigs[sigIdx], _sigs[sigIdx + 32]);
@@ -87,7 +93,7 @@ contract MiniSig {
             // recovered address.
             // If we exhaust the list without a match, revert
             // if we find a match, signerIdx = match index, continue looping through sigs
-            bool elem;
+            bool elem = false;
             for (uint256 j = signerIdx; j < signers.length; j++) {
                 if (addr == signers[j]) {
                     elem = true;
@@ -97,23 +103,17 @@ contract MiniSig {
             }
             require(elem, "not-signer");
             elem = false;
-
         }
 
-        uint256 endNonce = initialNonce + 1;
-        nonce = endNonce;
+        // TODO: return data?
         bool success;
         if (_callType == CallType.Call) {
             (success,) = _target.call{value: _value}(_data);
         } else {
-            // require(_value == 0)
+            // TODO: prevent delegatecall value confusion?
+            // require(_value == 0 || _value == msg.value)
             (success,) = _target.delegatecall(_data);
         }
         require(success, "call-failure");
-
-        // prevents changing the nonce storage slot (and reentrancy)
-        // this is less interesting in the solidity version, because we
-        // need to store signers anyway
-        require(nonce == endNonce, "oops");
     }
 }
